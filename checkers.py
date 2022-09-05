89b07e0da0b7e629e84960ee745d62521e42d64c
Tejas <tejas75o25@gmail.com>
2022-09-05 15:12:29 +0530

    Created the basic UI without the buttons or ai info

--- checkers.py
+++ checkers.py
@@ -8,12 +8,27 @@ pygame.init()
 
 BOARD_OFFSET = 30
 BOARD_SIZE = 640
+CELL_SIZE = BOARD_SIZE // 8
+BOARD_BORDER_THICKNESS = 20
+
 width, height = 1200, BOARD_SIZE + BOARD_OFFSET * 2
 pygame.display.set_caption("Checker AI")
 screen = pygame.display.set_mode((width, height))
-CELL_SIZE = BOARD_SIZE // 8
 
-background = pygame.Surface((BOARD_SIZE, BOARD_SIZE))
+background = pygame.Surface((width, height))
+background.fill(pygame.Color("#F4E7C6"))
+
+pygame.draw.rect(
+    background,
+    pygame.Color("#DCCFAE"),
+    (
+        BOARD_OFFSET - BOARD_BORDER_THICKNESS,
+        BOARD_OFFSET - BOARD_BORDER_THICKNESS,
+        BOARD_SIZE + BOARD_BORDER_THICKNESS * 2,
+        BOARD_SIZE + BOARD_BORDER_THICKNESS * 2,
+    ),
+    border_radius=10,
+)
 
 for file in range(8):
     for rank in range(8):
@@ -22,37 +37,35 @@ for file in range(8):
         pygame.draw.rect(
             background,
             color,
-            (file * CELL_SIZE, rank * CELL_SIZE, CELL_SIZE, CELL_SIZE),
+            (
+                file * CELL_SIZE + BOARD_OFFSET,
+                rank * CELL_SIZE + BOARD_OFFSET,
+                CELL_SIZE,
+                CELL_SIZE,
+            ),
         )
 
-BACKGROUND_BORDER_WIDTH = 20
-background_rect = background.get_rect(
-    topleft=(
-        BOARD_OFFSET - BACKGROUND_BORDER_WIDTH,
-        BOARD_OFFSET - BACKGROUND_BORDER_WIDTH,
-    )
+info_box_rect = pygame.Rect(
+    BOARD_OFFSET + BOARD_BORDER_THICKNESS + BOARD_SIZE + 10,
+    10,
+    490,
+    BOARD_SIZE + BOARD_BORDER_THICKNESS * 2,
 )
-background_rect.width += BACKGROUND_BORDER_WIDTH * 2
-background_rect.height += BACKGROUND_BORDER_WIDTH * 2
 
-font_manager = FontManager()
-font_manager.create_font("./assets/fonts/FiraCode-Bold.ttf", 40)
-font_manager.get_font(("./assets/fonts/FiraCode-Bold.ttf", 40)).set_underline(True)
+pygame.draw.rect(
+    background,
+    pygame.Color("#FEF1D0"),
+    info_box_rect,
+    border_radius=10,
+)
 
-title_text = font_manager.create_text(
-    ("./assets/fonts/FiraCode-Bold.ttf", 40), "Checker AI", (24, 48, 55)
-)
-title_text_rect = title_text.get_rect(
-    center=(background_rect.right + ((width - background_rect.right) / 2), 40)
-)
+info_box = pygame.image.load("./assets/images/info_section.png")
+background.blit(info_box, info_box_rect.topleft)
 
 clock = pygame.time.Clock()
 
 while True:
-    screen.fill(pygame.Color("#F4E7C6"))
-    pygame.draw.rect(screen, pygame.Color("#DCCFAE"), background_rect, border_radius=10)
-    screen.blit(background, (BOARD_OFFSET, BOARD_OFFSET))
-    screen.blit(title_text, title_text_rect.topleft)
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
